64c907b8552353e17b79af712cbf0758233da11d
David12307 <76618297+David12307@users.noreply.github.com>
2020-12-28 11:43:30 +0200

    I solved the error and changed the story :)

--- main.py
+++ main.py
@@ -1,49 +1,46 @@
 print("Welcome to my first game!")
 name = input("What is your name? ")
 age = int(input("What is your age? "))
+
 health = 10
 
-print("Hello", name, "you are", age, "years old.")
-
 if age >= 11:
-    print("You are old enough to play!")
-else:
-    print("You are not old enough to play...")
+  print("You are old enough to play!")
 
-wants_to_play = input("Do you want to play? (yes/no) ")
-if wants_to_play == "yes":
+  wants_to_play = input("Do you want to play? (yes/no) ")
+  if wants_to_play == "yes":
     print("You are starting with", health, "health.")
     print("Let's play!")
-if wants_to_play == "no":
-    print("Okay, you can close the game!")
+
+    left_or_right = input("First choice... Left or Right? (left/right) ")
+    if left_or_right == "left":
+      ans = input("Nice, you follow the path and reach a lake.. Do you swim across or go around? (across/around) ")
+
+      if ans == "around":
+          print("You went around and reached the other side of the lake")
+      elif ans == "across":
+          print("You managed to get across, but you were bit by a fish and lost 5 health.")
+          health -=5
+
+      ans = input("You notice a house and a river. Which do you go to? (river/house) ")
+      if ans == "house":
+          print("You go to the house and are greeted by the owner... He doesn't like you and you lose 5 health...")
+          health -= 5
+
+          if health <= 0:
+              print("You now have 0 health and you lose the game...")
+          else:
+              print("You have survived... You win!")
+
+      else:
+          print("You fell into the river and lost...")
 
 
-left_or_right = input("First choice... Left or Right? (left/right) ")
-if left_or_right == "left":
-    ans = input("Nice, you follow the path and reach a lake...  Do you swim across or go around? (across/around) ")
-if left_or_right == "right":
-    print("You fell down and lost...") #because it gives an error and i don't want to see it :/
-    print("You fell down and lost...") #because it gives an error and i don't want to see it :/
-    print("You fell down and lost...") #because it gives an error and i don't want to see it :/
-    print("You fell down and lost...") #because it gives an error and i don't want to see it :/
-    print("You fell down and lost...") #because it gives an error and i don't want to see it :/
-    print("You fell down and lost...") #because it gives an error and i don't want to see it :/
-    print("You fell down and lost...") #because it gives an error and i don't want to see it :/
-    print("You fell down and lost...") #because it gives an error and i don't want to see it :/
-    print("You fell down and lost...") #because it gives an error and i don't want to see it :/
+    else:
+      print("You fell down and lost...")
 
+  else:
+    print("Cya...")
+else:
+  print("You are not old enough to play...")
 
-
-if ans == "around":
-    ans = input("You made it around the lake and found a Cabin do you want to go in or continue your journey? (cabin/continue) ")
-if ans == "across":
-    print("You were killed by a shark...")
-    print("You lose...")
-
-if ans == "cabin":
-    print("You decided to go inside the Cabin, in there you found a Scary Lady that killed you...")
-    print("You lose...")
-
-if ans == "continue":
-    print("You find yourself in your cozy bed, your cat is laying next to you, you find out it was all a dream. A WILD Dream.")
-    print("You win!")
